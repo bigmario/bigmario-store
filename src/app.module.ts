8f9c7d4ab103ec28f304a397e570ea1f37dfc83e
Mario Castro <mariocastro.pva@gmail.com>
2022-05-15 13:48:55 -0400

    adding .env schema validations with Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
+import { Module, HttpModule, HttpService } from '@nestjs/common';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
-import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import { enviroments } from './enviroment';
+
+import config from './config';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -15,7 +20,13 @@ import { enviroments } from './enviroment';
     DatabaseModule,
     ConfigModule.forRoot({
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
   ],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
-  constructor(private config: ConfigService) {}
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
 
   Hello(): string {
-    const apiKey = this.config.get<string>('API_KEY');
-    const dbName = this.config.get<string>('DATABASE_NAME');
+    const apiKey = this.configService.apiKey;
+    const dbName = this.configService.database.name;
     return `Hello Mario! with token: ${apiKey} and DB: ${dbName}`;
   }
 }
