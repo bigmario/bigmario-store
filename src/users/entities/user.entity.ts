1461ea42f25e4e4bbd211487637605bd220fc470
Mario Castro <mariocastro.pva@gmail.com>
2022-05-28 14:30:24 -0400

    minor changes

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,13 @@
-import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+import { Customer } from 'src/customers/entities/customer.entity';
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
 @Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn()
@@ -12,4 +21,20 @@ export class User {
 
   @Column({ type: 'character varying', length: 255 })
   role: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
+
+  @OneToOne(() => Customer, (customer) => customer.user, { nullable: true })
+  @JoinColumn({ name: 'customer_id' })
+  customer: Customer;
 }
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -5,12 +5,15 @@ import { User } from 'src/users/entities/user.entity';
 import { Product } from 'src/products/entities/product.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from '../dto/user.dto';
+import { CustomersService } from 'src/customers/services/customer.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private userRepo: Repository<User>,
     @InjectRepository(Product) private productRepo: Repository<Product>,
+    private customersService: CustomersService,
     private configService: ConfigService,
   ) {}
 
@@ -26,10 +29,14 @@ export class UsersService {
     return user;
   }
 
-  // create(data: CreateUserDto) {
-  //   const newUser = new this.userModel(data);
-  //   return newUser.save();
-  // }
+  async create(data: CreateUserDto) {
+    const newUser = this.userRepo.create(data);
+    if (data.customerId) {
+      const customer = await this.customersService.findOne(data.customerId);
+      newUser.customer = customer;
+    }
+    return newUser.save();
+  }
 
   // update(id: string, changes: UpdateUserDto) {
   //   const user = this.userModel.findByIdAndUpdate(
